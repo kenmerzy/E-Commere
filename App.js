1b3048568d60911f865dcc7ecbb09bbfa1bc6c77
Nguyen Ngoc Long <kenmerzy@gmail.com>
2020-09-27 11:47:39 +0700

    add navigation

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React, { } from 'react'
 import { View, Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { LoginScreen, RegisterScreen, SplashScreen } from './src/screens'
+import { SCREEN_NAME } from './src/configs'
+
+const Stack = createStackNavigator()
 
 const App = () => {
   return (
-    <View>
-      <Text> App </Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name={SCREEN_NAME.SlashScreen} component={SplashScreen} />
+        <Stack.Screen name={SCREEN_NAME.LoginScreen} component={LoginScreen} />
+        <Stack.Screen name={SCREEN_NAME.RegisterScreen} component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
