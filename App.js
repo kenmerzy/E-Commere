8cd8d2877ef821b452e555109cc8f89035d4d779
Nguyen Ngoc Long <kenmerzy@gmail.com>
2020-09-27 12:36:06 +0700

    add redux and persit

--- App.js
+++ App.js
@@ -2,24 +2,28 @@ import React, { } from 'react'
 import { View, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { Provider } from 'react-redux'
 import { LoginScreen, RegisterScreen, SplashScreen } from './src/screens'
+import store from './src/redux/store'
 import { SCREEN_NAME } from './src/configs'
 
 const Stack = createStackNavigator()
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name={SCREEN_NAME.SlashScreen} component={SplashScreen} />
-        <Stack.Screen name={SCREEN_NAME.LoginScreen} component={LoginScreen} />
-        <Stack.Screen name={SCREEN_NAME.RegisterScreen} component={RegisterScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name={SCREEN_NAME.SlashScreen} component={SplashScreen} />
+          <Stack.Screen name={SCREEN_NAME.LoginScreen} component={LoginScreen} />
+          <Stack.Screen name={SCREEN_NAME.RegisterScreen} component={RegisterScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   )
 }
 
