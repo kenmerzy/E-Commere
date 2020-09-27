0702a0f3a298acd95f2ea46396ff7b88592156aa
Nguyen Ngoc Long <kenmerzy@gmail.com>
2020-09-27 21:09:06 +0700

    add redux saga

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -1,8 +1,9 @@
 import { userTypes } from '../types'
 
 export const registerUser = (data) => {
-  return (
-    type: userTypes.REGISTER_USER,
+  return ({
+    type: userTypes.REGISTER_USER_SUCCESS,
     payload: { data },
+  }
   )
 }
